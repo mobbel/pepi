2185832fb987be7d444df3fd52d39dcf9d759d71
Stephan Sperling <sperling.stephan@googlemail.com>
2017-04-04 20:13:40 +0200

    create logging setup

--- pepi.pl
+++ pepi.pl
@@ -1,9 +1,17 @@
 #! /usr/bin/perl
 
 BEGIN {
-  use Log::Log4Perl;
+  use Path::Tiny;
+  my $s_pepiRootDirectory = path($0)->parent.'/';
+
+  my $s_setupDirectory = $s_pepiRootDirectory . 'setup/';
+
+  use Log::Log4perl;
+  Log::Log4perl->init($s_setupDirectory.'log.setup');
 }
 
 sub main {
 
 }
+
+main();
