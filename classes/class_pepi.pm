197a33b2e45e4c4eca55975e2cc9917067c9c71c
Stephan Sperling <sperling.stephan@googlemail.com>
2017-04-06 19:25:48 +0200

    extends log-system

--- classes/class_pepi.pm
+++ classes/class_pepi.pm
@@ -1,9 +1,28 @@
+use strict;
+use warnings;
+
 package classes::class_pepi;
 
 sub new {
   my ($or_self, $hr_variables) = @_;
 
+  $hr_variables->{config} = {};
+
   bless $hr_variables, $or_self;
+}
+
+sub setConfiguration {
+  my ($or_self, $configuration) = @_;
+
+  $or_self->{config} = $configuration;
+}
+
+sub readFiles {
+
+}
+
+sub writeFiles {
+
 }
 
 return 1;
--- pepi.pl
+++ pepi.pl
@@ -1,17 +1,26 @@
 #! /usr/bin/perl
 
+use strict;
+use warnings;
+
 BEGIN {
   use Path::Tiny;
-  my $s_pepiRootDirectory = path($0)->parent.'/';
+  my $s_prj = path(__FILE__)->absolute->parent->parent.'/';
 
+  my $s_pepiRootDirectory = $s_prj.'pepi/';
   my $s_setupDirectory = $s_pepiRootDirectory . 'setup/';
 
   use Log::Log4perl;
   Log::Log4perl->init($s_setupDirectory.'log.setup');
+
+  unshift @INC, $s_pepiRootDirectory;
 }
 
 sub main {
+  require classes::class_pepi;
+  my $or_pepi = new classes::class_pepi({});
 
+  $or_pepi->setConfiguration('test')
 }
 
 main();
